7d720b448c0d885eefea07543d5da0a1da52b0a8
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2022-08-29 21:31:15 +0200

    Character's animation movement.

--- Assets/Scripts/HeroCharacterScript.cs
+++ Assets/Scripts/HeroCharacterScript.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class HeroCharacterScript : MonoBehaviour
 {
     // Variables
+    public SpriteRenderer spritRenderer;
+    
     public float moveSpeed = 5.0f;
 
     public Rigidbody2D rigidBody;
 
-    public Vector2 direction; 
-    
+    public Vector2 direction;
+
+    public Animator animator;
+
+    private int animationState = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,27 +26,41 @@ public class HeroCharacterScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        HandleMovement();
+        
+        rigidBody.MovePosition(rigidBody.position + direction * (moveSpeed * Time.fixedDeltaTime));
+        
+        animator.SetInteger("dir", animationState);
+    }
+
+    private void HandleMovement()
+    {
         if (Input.GetKey(KeyCode.UpArrow))
         {
             direction = Vector2.up;
+            animationState = 1;
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
             direction = Vector2.down;
+            animationState = 3;
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             direction = Vector2.left;
+            animationState = 2;
+            spritRenderer.flipX = false;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             direction = Vector2.right;
+            animationState = 2;
+            spritRenderer.flipX = true;
         }
         else
         {
             direction = Vector2.zero;
+            animationState = 0;
         }
-        
-        rigidBody.MovePosition(rigidBody.position + direction * (moveSpeed * Time.fixedDeltaTime));
     }
 }
