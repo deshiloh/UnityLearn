e1c29caa1841398899b84d76377a0ba48638b741
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2022-09-09 16:49:54 +0200

    wip

--- Assets/Scripts/HeroCharacterScript.cs
+++ Assets/Scripts/HeroCharacterScript.cs
@@ -72,35 +72,4 @@ public class HeroCharacterScript : MonoBehaviour
     {
         rigidBody.MovePosition(rigidBody.position + direction * (moveSpeed * Time.fixedDeltaTime));
     }
-
-    private void HandleMovement()
-    {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            direction = Vector2.up;
-            _animationState = 1;
-        }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            direction = Vector2.down;
-            _animationState = 3;
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            direction = Vector2.left;
-            _animationState = 2;
-            spritRenderer.flipX = false;
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            direction = Vector2.right;
-            _animationState = 2;
-            spritRenderer.flipX = true;
-        }
-        else
-        {
-            direction = Vector2.zero;
-            _animationState = 0;
-        }
-    }
 }
